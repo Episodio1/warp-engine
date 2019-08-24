9ee01191d1dd11ee44e1a209218d41a4ca815081
Matias Anoniz <matiasanoniz@gmail.com>
2019-08-24 00:14:08 -0300

    Add tag --remove-orphans on start containers

--- .warp/bin/start.sh
+++ .warp/bin/start.sh
@@ -47,19 +47,19 @@ function start() {
 
         if [ ! -z $CUSTOM_YML_FILE ] ; then
           # start docker with custom yml file
-          docker-compose -f $DOCKERCOMPOSEFILE -f $DOCKERCOMPOSEFILEMAC -f $CUSTOM_YML_FILE up -d
+          docker-compose -f $DOCKERCOMPOSEFILE -f $DOCKERCOMPOSEFILEMAC -f $CUSTOM_YML_FILE up --remove-orphans -d
         else
           # start docker containers in macOS
-          docker-compose -f $DOCKERCOMPOSEFILE -f $DOCKERCOMPOSEFILEMAC up -d
+          docker-compose -f $DOCKERCOMPOSEFILE -f $DOCKERCOMPOSEFILEMAC up --remove-orphans -d
         fi
       ;;
       Linux)
         if [ ! -z $CUSTOM_YML_FILE ] ; then
           # start docker with custom yml file
-          docker-compose -f $DOCKERCOMPOSEFILE -f $CUSTOM_YML_FILE up -d
+          docker-compose -f $DOCKERCOMPOSEFILE -f $CUSTOM_YML_FILE up --remove-orphans -d
         else
           # start docker containers in linux
-          docker-compose -f $DOCKERCOMPOSEFILE up -d
+          docker-compose -f $DOCKERCOMPOSEFILE up --remove-orphans -d
         fi
       ;;
     esac
