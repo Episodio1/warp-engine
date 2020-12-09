292d93a864eedd37ab300d8de65c6230fd7d79c6
Mauricio Barchiesi <barchiesi.mauricio@gmail.com>
2020-12-09 10:00:56 -0300

    ES simil SSH

--- .warp/bin/elasticsearch.sh
+++ .warp/bin/elasticsearch.sh
@@ -15,6 +15,7 @@ function elasticsearch_info()
     ES_HOST="elasticsearch"
     ES_VERSION=$(warp_env_read_var ES_VERSION)
     ES_MEMORY=$(warp_env_read_var ES_MEMORY)
+    ES_HOST2CONTAINER_PORT=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "9200/tcp") 0).HostPort}}' $(warp docker ps -q elasticsearch))
 
     MODE_SANDBOX=$(warp_env_read_var MODE_SANDBOX)
 
@@ -30,7 +31,7 @@ function elasticsearch_info()
         warp_message_info "* Elasticsearch"
         warp_message "Version:                    $(warp_message_info $ES_VERSION)"
         warp_message "Host:                       $(warp_message_info $ES_HOST)"
-        warp_message "Ports (container):          $(warp_message_info '9200, 9300')"
+        [[ -n $ES_HOST2CONTAINER_PORT ]] && warp_message "Ports (container):          $(warp_message_info "$ES_HOST2CONTAINER_PORT --> 9200")"
         warp_message "Data:                       $(warp_message_info $PROJECTPATH/.warp/docker/volumes/elasticsearch)"
         warp_message "Memory:                     $(warp_message_info $ES_MEMORY)"
 
@@ -50,16 +51,91 @@ function elasticsearch_command()
 
 }
 
+elasticsearch-simil_ssh() {
+    : '
+    This function provides a bash pipe as root or elasticsearch user.
+    It is called as SSH in order to make it better for developers ack
+    but it does not use Secure Shell anywhere.
+    '
+
+    # Check for wrong input:
+    if [[ $# -gt 1 ]]; then
+        elasticsearch-ssh_wrong_input
+        exit 1
+    else
+        if [[ $1 == "--root" ]]; then
+            # Check if warp is running:    
+            if [ $(warp_check_is_running) = false ]; then
+                warp_message_error "The containers are not running"
+                warp_message_error "please, first run warp start"
+                exit 1
+            fi
+            docker-compose -f $DOCKERCOMPOSEFILE exec -u root elasticsearch bash
+        elif [[ -z $1 || $1 == "--elasticsearch" ]]; then
+            # Check if warp is running:    
+            if [ $(warp_check_is_running) = false ]; then
+                warp_message_error "The containers are not running"
+                warp_message_error "please, first run warp start"
+                exit 1
+            fi
+            # It is better if defines elasticsearch user as default ######################
+            docker-compose -f $DOCKERCOMPOSEFILE exec -u elasticsearch elasticsearch bash
+        elif [[ $1 == "-h" || $1 == "--help" ]]; then
+            elasticsearch-ssh_help
+            exit 0
+        else
+            elasticsearch-ssh_wrong_input
+            exit 1
+        fi
+    fi
+}
+
+elasticsearch-clear_all() {
+    : '
+    This function unlocks and delete all indexes.
+    '
+
+    # Parsing ES dynamic binded port:
+    ES_HOST2CONTAINER_PORT=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "9200/tcp") 0).HostPort}}' $(warp docker ps -q elasticsearch))
+    # Unlocking indexes:
+    ACK=$(curl --no-progress-meter -X PUT -H "Content-Type: application/json" http://localhost:$ES_HOST2CONTAINER_PORT/_all/_settings -d '{"index.blocks.read_only_allow_delete": null}')
+    if [[ $(echo "$ACK" | grep '{"acknowledged":true}') ]]; then
+        warp_message "* Unlocking indexes... $(warp_message_ok [ok])"
+        # Deleting indexes:
+        ACK=$(curl --no-progress-meter -X DELETE "localhost:$ES_HOST2CONTAINER_PORT/_all")
+        if [[ $(echo "$ACK" | grep '{"acknowledged":true}') ]]; then
+            warp_message "* Deleting indexes... $(warp_message_ok [ok])"
+        else
+            warp_message_error "Delete process fail"
+            exit 1
+        fi
+    else
+      warp_message_error "Unlock process fail"
+      exit 1
+    fi
+}
+
 function elasticsearch_main()
 {
     case "$1" in
         elasticsearch)
-		      shift 1
-          elasticsearch_command $*  
+		    shift 1
+            elasticsearch_command $*  
+        ;;
+
+        --clear-all)
+            shift
+            elasticsearch-clear_all
         ;;
 
         info)
+            shift
             elasticsearch_info
+        ;;
+
+        ssh)
+            shift
+            elasticsearch-simil_ssh $*
         ;;
 
         -h | --help)
@@ -70,4 +146,10 @@ function elasticsearch_main()
 		    elasticsearch_help_usage
         ;;
     esac
+}
+
+elasticsearch-ssh_wrong_input() {
+    warp_message_error "Wrong input."
+    elasticsearch-ssh_help
+    exit 1
 }
